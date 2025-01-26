eb3b0fe871d1ce6913ff80db38c532db94ccf351
gonzalezem <gonzalezem@msoe.edu>
2025-01-26 16:27:19 -0600

    Added board functionality.

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,3 +1,46 @@
+import React, { useState } from "react"
+
+function Board(): React.ReactNode {
+    enum Player {
+        X = "player1",
+        O = "player2"
+    }
+    interface GameState {
+        nextPlayer: Player; 
+        isOngoing: boolean;
+    }
+
+    
+    const [state, setState] = useState<GameState>({
+        nextPlayer: Player.X,
+        isOngoing: true
+    })
+    let nextState: SquareState = state.nextPlayer === Player.X ? SquareState.Player1 : SquareState.Player2;
+
+    function onClick() {
+        setState({
+            nextPlayer: state.nextPlayer === Player.X ? Player.O : Player.X,
+            isOngoing: true
+        })
+        console.log(state)
+        nextState = state.nextPlayer === Player.X ? SquareState.Player1 : SquareState.Player2;
+    }
+
+    return (
+        <div id="board">
+            <Square state={SquareState.Default} nextState={nextState} onClick={onClick}/>
+            <Square state={SquareState.Default} nextState={nextState} onClick={onClick}/>
+            <Square state={SquareState.Default} nextState={nextState} onClick={onClick}/>
+            <Square state={SquareState.Default} nextState={nextState} onClick={onClick}/>
+            <Square state={SquareState.Default} nextState={nextState} onClick={onClick}/>
+            <Square state={SquareState.Default} nextState={nextState} onClick={onClick}/>
+            <Square state={SquareState.Default} nextState={nextState} onClick={onClick}/>
+            <Square state={SquareState.Default} nextState={nextState} onClick={onClick}/>
+            <Square state={SquareState.Default} nextState={nextState} onClick={onClick}/>
+        </div>
+    )
+}
+
 enum SquareState {
     Default = "default",
     Disabled = "disabled",
@@ -5,25 +48,20 @@ enum SquareState {
     Player2 = "player2"
 }
 
-type SquareProps = {
+interface SquareProps {
     state: SquareState;
+    nextState: SquareState;
     onClick: () => void;
 }
 
-function Board(): React.ReactNode {
-    function onClick() {
-        throw new Error('Function not implemented.')
+function Square(props: SquareProps): React.ReactNode {
+    const [state, setState] = useState<SquareState>(props.state)
+
+    function handleClick(): void {
+        props.onClick()
+        setState(props.nextState)
     }
 
-    return (
-        <div id="board">
-            <Square state={SquareState.Player2} onClick={onClick}/>
-        </div>
-    )
-}
-
-
-function Square({ state, onClick }: SquareProps): React.ReactNode {
     function renderState(state: SquareState): string {
         switch (state) {
             case SquareState.Default:
@@ -37,7 +75,7 @@ function Square({ state, onClick }: SquareProps): React.ReactNode {
     }
 
     return (
-        <button className="square" onClick={onClick}>
+        <button className="square" onClick={handleClick}>
             <span>{renderState(state)}</span>
         </button>
     )
